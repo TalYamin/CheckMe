8c6e8b019fba26e000ab5d679407ff1f892fa9af
user <user@DESKTOP-RDC336S.mynet>
2020-03-17 13:13:15 +0200

    Update RestTable

--- checkme/src/main/java/com/checkme/model/RestTable.java
+++ checkme/src/main/java/com/checkme/model/RestTable.java
@@ -1,9 +1,8 @@
 package com.checkme.model;
 
+import java.time.LocalDate;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 import javax.persistence.Basic;
 import javax.persistence.CascadeType;
@@ -17,8 +16,11 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.MapKey;
+import javax.persistence.Transient;
 
 import org.springframework.stereotype.Component;
+
+import com.checkme.utils.DateConverterUtil;
 
 @Component
 @Entity
@@ -28,21 +30,27 @@ public class RestTable {
 	
 	private boolean active;
 	
+	private LocalDate tableDate;
+	
 	private Map<Long, Customer> customers = new HashMap<Long, Customer>();
 	
 	private Restaurant restaurant;
 	
 	private Map<Long, MenuElement> menuElements = new HashMap<Long, MenuElement>();
 	
+	@Transient
+	private String customDate;
 	
 	public RestTable() {
 		
 	}
 	
-	public RestTable(boolean active, Restaurant restaurant) {
+	public RestTable(boolean active, String tableDate, Restaurant restaurant) {
 		
 		setActive(active);
+		setTableDate(DateConverterUtil.convertStringDate(tableDate));
 		setRestaurant(restaurant);
+		
 	}
 
 
@@ -68,6 +76,16 @@ public class RestTable {
 	public void setActive(boolean active) {
 		this.active = active;
 	}
+	
+	@Basic(optional = false)
+	@Column(nullable = false)
+	public LocalDate getTableDate() {
+		return tableDate;
+	}
+
+	public void setTableDate(LocalDate tableDate) {
+		this.tableDate = tableDate;
+	}
 
 	@ManyToMany(mappedBy = "tables")
 	@MapKey(name="id")
@@ -90,10 +108,6 @@ public class RestTable {
 	}
 	
 	
-	@Override
-	public String toString() {
-		return "RestTable [id=" + id + ", active=" + active + "]";
-	}
 
 	@ManyToMany(cascade = { CascadeType.ALL }, fetch = FetchType.EAGER)
 	@MapKey(name = "id")
@@ -108,14 +122,25 @@ public class RestTable {
 	}
 
 	@Override
+	public String toString() {
+		
+		customDate = DateConverterUtil.DateStringFormat(this.tableDate);
+		
+		return "RestTable [id=" + id + ", active=" + active + ", tableDate=" + customDate + ", restaurant=" + restaurant
+				+ "]";
+	}
+
+	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + (active ? 1231 : 1237);
+		result = prime * result + ((customDate == null) ? 0 : customDate.hashCode());
 		result = prime * result + ((customers == null) ? 0 : customers.hashCode());
 		result = prime * result + (int) (id ^ (id >>> 32));
 		result = prime * result + ((menuElements == null) ? 0 : menuElements.hashCode());
 		result = prime * result + ((restaurant == null) ? 0 : restaurant.hashCode());
+		result = prime * result + ((tableDate == null) ? 0 : tableDate.hashCode());
 		return result;
 	}
 
@@ -129,6 +154,11 @@ public class RestTable {
 			return false;
 		RestTable other = (RestTable) obj;
 		if (active != other.active)
+			return false;
+		if (customDate == null) {
+			if (other.customDate != null)
+				return false;
+		} else if (!customDate.equals(other.customDate))
 			return false;
 		if (customers == null) {
 			if (other.customers != null)
@@ -147,8 +177,15 @@ public class RestTable {
 				return false;
 		} else if (!restaurant.equals(other.restaurant))
 			return false;
+		if (tableDate == null) {
+			if (other.tableDate != null)
+				return false;
+		} else if (!tableDate.equals(other.tableDate))
+			return false;
 		return true;
 	}
+
+	
 
 	
 
